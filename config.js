160f05d68320631a31911b675f53d7c7c3c6e1db
apheliolab <apheliolab@gmail.com>
2026-04-13 15:43:51 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APHELIO_CONFIG = {
-  webhookUrl: "https://aphelio-landing-n8n.wxugbb.easypanel.host/webhook/formul%C3%A1rio",
+  webhookUrl: "https://aphelio-landing-n8n.wxugbb.easypanel.host/webhook-test/formulário",
 };
