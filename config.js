071158c3927201f8060de7b2f454e4a22bbcbbcf
apheliolab <apheliolab@gmail.com>
2026-04-15 16:44:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.APHELIO_CONFIG = {
-  webhookUrl: "https://aphelio-landing-n8n.wxugbb.easypanel.host/webhook/formul%C3%A1rio",
+  webhookUrl: "https://aphelio-n8n.6pecl2.easypanel.host/webhook-test/fe103405-eaab-47cd-9968-a98aced30654",
   whatsappRedirectUrl: "https://wa.me/5535999096959",
 };
